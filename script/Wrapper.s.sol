3045325bf64c2205c8311d555d55b6643c29bb93
virgil2019 <danten0402@gmail.com>
2025-12-30 15:46:13 +0800

    build: update deploy script

--- script/Wrapper.s.sol
+++ script/Wrapper.s.sol
@@ -7,8 +7,12 @@ import "../src/Wrapper.sol";
 contract WrapperDeploy is Script {
     function run() public {
         uint256 deployer = vm.envUint("PRIVATE_KEY");
+        string memory tokenName = vm.envString("TOKEN_NAME");
+        string memory tokenVersion = vm.envString("TOKEN_VERSION");
         vm.startBroadcast(deployer);
         Wrapper wrapper = new Wrapper(
+            tokenName,
+            tokenVersion
         );
         wrapper.setTokenAddress(vm.envAddress("REAL_TOKEN_ADDRESS"));
         vm.stopBroadcast();
